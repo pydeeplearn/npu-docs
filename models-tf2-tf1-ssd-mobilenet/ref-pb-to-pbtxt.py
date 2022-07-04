8a35824cfe9d1c579f9e62eead98402444db6a99
minghuadev <minghuadev@users.noreply.github.com>
2022-07-03 17:56:40 -0700

    models-tf2-tf1-ssd-mobilenet directory

---
+++ models-tf2-tf1-ssd-mobilenet/ref-pb-to-pbtxt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# ref-pb-to-pbtxt.py
+#   convert pb to pbtxt for tf 2.xxx
+#   https://stackoverflow.com/questions/57007707/failed-to-convert-tensorflow-frozen-graph-to-pbtxt-file
+
+
+# use tensorflow 2.9.1
+
+
+import tensorflow as tf
+from google.protobuf import text_format
+from tensorflow.python.platform import gfile
+
+def graphdef_to_pbtxt(filename): 
+    with open(filename,'rb') as f:
+        graph_def = tf.compat.v1.GraphDef()
+        graph_def.ParseFromString(f.read())
+    with open('protobuf.txt', 'w') as fp:
+        fp.write(str(graph_def))
+    
+graphdef_to_pbtxt('saved_model.pb')
+
+
